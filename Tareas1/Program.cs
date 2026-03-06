bb45ee706f39953ff35fe0f355b125629d75b373
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-05 22:15:50 -0600

    ejercicio2

--- Tareas1/Program.cs
+++ Tareas1/Program.cs
@@ -1,8 +1,19 @@
 ﻿//inicio del ejecicios
 //primer ejercicio 
+/*
 int numero = 1;
 do
 {
     Console.WriteLine(numero);
     numero++;
 }while (numero <= 5);
+*/
+//ejercicio 2
+
+ int numero = 1;
+do
+{
+    Console.WriteLine(numero);
+    numero++;
+}while (numero <= 10);
+
